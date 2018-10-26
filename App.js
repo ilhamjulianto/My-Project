f1c1c1b5ad63d9fef2c67cd359565c502c977ec5
ilhamjulianto <40849246+ilhamjulianto@users.noreply.github.com>
2018-10-27 06:27:34 +0700

    Update App.js

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Navigator from './app/Routes/Navigator'
 
 class App extends React.Component {
+  //;lkjhgfdsdfghj,hgzdfghjkl;kjhgfxzxfghjkl;lkjhgfdsdfghjkl;lkjhgf
   render() {
     return (
         <Navigator />
